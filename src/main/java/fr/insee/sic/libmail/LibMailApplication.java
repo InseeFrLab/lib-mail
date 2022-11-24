1948dca0a0103029616017c588742f1855c7f92b
y72wvh <betty.becuwe@insee.fr>
2022-11-24 14:43:34 +0100

    Initial commit

---
+++ src/main/java/fr/insee/sic/libmail/LibMailApplication.java
@@ -0,0 +1,13 @@
+package fr.insee.sic.libmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/insee/sic/libmail/LibMailApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insee.sic.libmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
